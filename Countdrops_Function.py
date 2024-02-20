8e1b581860f473b0d5a333c942aa23d346e5c46f
Allec Terrezo <38410987+AllecTerrezo@users.noreply.github.com>
2024-02-19 21:20:31 -0300

    Adjustments made Zeno and Countdrops

--- Countdrops_Function.py
+++ Countdrops_Function.py
@@ -1,34 +1,38 @@
 import math 
 
-def count_drops(floors: int, eggs: int) -> int:
+def count_drops(limit_floor: int, eggs: int) -> int:
 
-# Number of covered floors: x + (x-1) + (x-2) + ... + 1 = x*(x+1)/2 
-
-# Covered floors <= floors 
-
-# x*(x+1)/2 <= floors 
-
-# x^2 + x - 2*floors <= 0 
-
-# With quadratic formula ((-b + sqrt(b^2 -4ac))/2): x = (-1 + sqrt(1 + 8*floors)) / 2 
-
-    number_drops = (math.sqrt(1 + 8*floors) -1) // 2
-
-# Number of floors covered by drops
+    if limit_floor>100:
+        return "Limit above the allowed, please pass a number equal or bellow 100."
     
-    covered_floors = number_drops * (number_drops + 1) // 2
+    else:
 
-#Using the first egg to understand how many floors are left and verifying if there are eggs and floors we can sum the Number of drops in the current attempt with the result of calling the count_drops function recursively
+        # Number of covered floors (Arithmetic Progression Sum of floors per egg used): k + (k-1) + (k-2) + ... + 1 = k*(k+1)/2 
 
-    left_floors = floors - covered_floors
-    eggs_quantity = eggs - 1
-    if eggs_quantity > 0 and left_floors > 0:
-        return int(number_drops + count_drops(left_floors, eggs_quantity))
+        # Covered floors <= limit_floor 
 
-# If eggs_quantity or left_floors equals 0 return the value of Number of drops
+        # k*(k+1)/2 <= limit_floor 
 
-    return int(number_drops)
+        # k^2 + k - 2*limit_floor <= 0 
 
-#Based on the question where we have the limit floor and 2 eggs.
+        # Using Bhaskara formula to discover positive k. ((-b + sqrt(b^2 -4ac))/2): k = (-1 + sqrt(1 + 8*floors)) / 2 
 
-print(count_drops(83, 2))
+            number_drops = (math.sqrt(1 + 8*limit_floor) -1) // 2
+
+        # Number of floors covered by drops
+            
+            covered_floors = number_drops * (number_drops + 1) // 2
+
+        #Using the lost egg we can understand how many floors are left and verifying if there are eggs and floors we can sum the Number of drops in the current attempt with the result of calling the count_drops function recursively
+
+            left_floors = limit_floor - covered_floors
+            eggs_quantity = eggs - 1
+            if eggs_quantity > 0 and left_floors > 0:
+                return int(number_drops + count_drops(left_floors, eggs_quantity))
+
+        # If eggs_quantity or left_floors equals 0 return the value of Number of drops
+
+            return int(number_drops)
+
+#Example
+print(count_drops(63, 2))
--- Zeno_Paradox.py
+++ Zeno_Paradox.py
@@ -4,10 +4,10 @@ import time
 
 def zeno_paradox(): 
     achilles_position = 0 
-    tortoise_position = 50 
+    tortoise_position = 45.1 
     achilles_velocity = 10
     tortoise_velocity = 1 
-    timeframe = 1 # Time frame will be lower each loop iteraction in order to generate the paradox perception
+    time_passed = 1 # time_passed will be lower each loop iteraction in order to generate the paradox perception
     Turn = 0
 
     # While tortoise_position greater than achilles keep looping
@@ -16,14 +16,16 @@ def zeno_paradox():
         # Update speed, position and timeframe (to give Zeno's perception)
         achilles_position += achilles_velocity 
         tortoise_position += tortoise_velocity
-        achilles_velocity = achilles_velocity//timeframe 
-        tortoise_velocity = tortoise_velocity//timeframe 
-        timeframe = timeframe+0.0626
+        achilles_velocity = achilles_velocity//time_passed 
+        tortoise_velocity = tortoise_velocity//time_passed 
+        time_passed = time_passed+0.0626
         Turn +=1
+        
         # Print the positions graphically and give a delay to give an animation perception
         print("\n\nRound: ", Turn)
-        print("\n \r Achilles: [{}]".format("#" * int(achilles_position)))
-        print("\n \r Tortoise: [{}]".format("-" * int(tortoise_position)))
+        print("\nTime passed: ", time_passed)
+        print("\n \r Achilles: {}".format(achilles_position))
+        print("\n \r Tortoise: {}".format(tortoise_position))
         time.sleep(0.4) 
 
 # Call the function to run the simulation 
